ada59f11d736344a200f28f6c4e60a29ee135332
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-06 17:15:03 -0600

    COmpleted task - Connect a better pre-made action.

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,13 @@ import CalcButton from './CalcButton';
 
 import reducer, { initialState } from '../reducers';
 
-import { addOne } from '../actions';
+import { applyNumber } from '../actions';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleClick = () => {
-    dispatch(addOne());
+  const handleClick = (number) => {
+    dispatch(applyNumber(number));
   }
 
   return (
@@ -39,21 +39,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={handleClick} />
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleClick(1)} />
+              <CalcButton value={2} onClick={() => handleClick(2)} />
+              <CalcButton value={3} onClick={() => handleClick(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleClick(4)} />
+              <CalcButton value={5} onClick={() => handleClick(5)} />
+              <CalcButton value={6} onClick={() => handleClick(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleClick(7)} />
+              <CalcButton value={8} onClick={() => handleClick(8)} />
+              <CalcButton value={9} onClick={() => handleClick(9)} />
             </div>
 
             <div className="row">
